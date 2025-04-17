b30a2f1b46b39dfc980969bda334dea134706917
Louis Darko <60629659+LouisDarko@users.noreply.github.com>
2025-04-17 23:44:10 +0900

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -7,6 +7,38 @@ from gtts import gTTS
 import base64
 import os
 
+# Navigation Bar
+st.markdown("""
+    <style>
+        .nav-menu {
+            background-color: #4C9D70;
+            padding: 15px;
+            border-radius: 12px;
+            display: flex;
+            justify-content: center;
+            gap: 40px;
+            font-size: 18px;
+            margin-bottom: 40px;
+        }
+        .nav-menu a {
+            color: white;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        .nav-menu a:hover {
+            text-decoration: underline;
+        }
+    </style>
+
+    <div class='nav-menu'>
+        <a href='/Home' target='_self'>🏠 Home</a>
+        <a href='/Risk_Assessment' target='_self'>📝 Risk Assessment</a>
+        <a href='/Results' target='_self'>📊 Results</a>
+        <a href='/Recommendations' target='_self'>🤝🏾 Recommendations</a>
+    </div>
+""", unsafe_allow_html=True)
+
+
 # Page title and layout
 st.set_page_config(page_title="Stroke Risk Results", layout="wide")
 st.title("📊 Stroke Risk Results")
