59a898c10296870ac64ac9dfabb38e264f6047b8
Louis Darko <60629659+LouisDarko@users.noreply.github.com>
2025-04-17 20:44:36 +0900

    Delete pages/recommendations.py

--- pages/recommendations.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def app(lang):
-    st.header("💡 " + ("Health Recommendations" if lang == "English" else "Recommandations de Santé"))
-    if lang == "English":
-        st.markdown("- Exercise 🏃‍♂️\n- Eat healthy 🥦\n- Avoid smoking 🚭\n- Monitor blood pressure and sugar 🩸")
-    else:
-        st.markdown("- Faire de l'exercice 🏃‍♂️\n- Manger sainement 🥦\n- Éviter de fumer 🚭\n- Surveiller la tension et le sucre 🩸")
