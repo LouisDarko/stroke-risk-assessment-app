694576f4e58239234936e54c46642754aba1e2b7
Louis Darko <60629659+LouisDarko@users.noreply.github.com>
2025-04-17 23:44:29 +0900

    Update Recommendations.py

--- pages/Recommendations.py
+++ pages/Recommendations.py
@@ -1,5 +1,37 @@
 import streamlit as st
 
+# Navigation Bar
+st.markdown("""
+    <style>
+        .nav-menu {
+            background-color: #4C9D70;
+            padding: 15px;
+            border-radius: 12px;
+            display: flex;
+            justify-content: center;
+            gap: 40px;
+            font-size: 18px;
+            margin-bottom: 40px;
+        }
+        .nav-menu a {
+            color: white;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        .nav-menu a:hover {
+            text-decoration: underline;
+        }
+    </style>
+
+    <div class='nav-menu'>
+        <a href='/Home' target='_self'>🏠 Home</a>
+        <a href='/Risk_Assessment' target='_self'>📝 Risk Assessment</a>
+        <a href='/Results' target='_self'>📊 Results</a>
+        <a href='/Recommendations' target='_self'>🤝🏾 Recommendations</a>
+    </div>
+""", unsafe_allow_html=True)
+
+
 # Page configuration
 st.set_page_config(page_title="Stroke Risk Recommendations", layout="wide")
 st.title("💡 Stroke Prevention Recommendations")
